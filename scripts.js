65385f286bff98bd55c39eeff4304d32daf389bf
John LaDeur <jdladeur@gmail.com>
2020-02-22 19:17:44 -0600

    format HTML

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
-console.log('hello world!')
+//when Add Expense is clicked, grab default-message from html and set display to none
+//grab values from Type, amount-entry, name-entry, date-entry, location-entry
+//create table data in "item-row"
+//return input values in "item-row"
